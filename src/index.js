35951fa7361c54f1c61438ca2c8280c89e5e670c
Lixang Li <gtlx54@gmail.com>
2025-10-17 14:09:02 +1100

    Fixed index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import {createToDoItem} from './todo'
 import {createProject} from './projects'
 import {isThereACurrentProject,getProject,saveProject}  from './localStorage'
+import {displayToDo,show} from './ui'
+
+
+function defaultProject() {
+    let newProject = createProject("New Project");
+    newProject.createToDoItem("New Todo", "Description");
+    return newProject;
+}
+
 //check localstorage or else create new project
 
 
-// check local storage for project 
-if isThereACurrentProject() 
-// else create new
-createProject("Hello From New Project")
+// check local storage for project // else create new
+let currentProject = isThereACurrentProject() ? getProject() : defaultProject();
+
+show(currentProject);
+
+
